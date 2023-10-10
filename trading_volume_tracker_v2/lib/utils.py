2d777be07ca9f847f9ace0451ba75a3ef477eb67
davidting0918 <dting0918@gmail.com>
2023-10-10 23:16:08 +0800

    Update listing db and listing job

--- trading_volume_tracker_v2/lib/utils.py
+++ trading_volume_tracker_v2/lib/utils.py
@@ -36,10 +36,13 @@ class BaseClient:
         logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s %(name)s %(message)s")
 
 
-class CMCGrabber(BaseClient):
+class Grabber(BaseClient):
     CMC_API_KEY = "CMC_API_KEY"
     BLACK_LIST = "BLACK_LIST"
+    EXCHANGE_INFO = "EXCHANGE_INFO"
     TOKEN_INFO_URL = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest"
+    
+    WOO_LISTING_URL = "https://api.woo.org/v1/public/info"
 
     def __init__(self):
         self._init_logger()
@@ -47,23 +50,47 @@ class CMCGrabber(BaseClient):
 
     @staticmethod
     def _handle_response(response: dict, category: str, **kwargs):
-        if category == "token_info":
-            token_info = pd.DataFrame(response["data"])[["id", "name", "slug", "symbol", "cmc_rank"]].loc[
-                : kwargs["num"]
-            ]
-            token_info["date"] = dt.today().date()
-            return token_info
-        elif category == "volume":
-            try:
-                volume = pd.DataFrame(response["data"]["marketPairs"])[["volumeUsd"]]
-                volume.columns = [kwargs["symbol"]]
-                return volume
-            except KeyError as e:
-                logging.warning(f"{kwargs['symbol']} KeyError: {e}")
-                return pd.DataFrame()
+        try:
+            if category == "token_info":
+                token_info = pd.DataFrame(response["data"])[["id", "name", "slug", "symbol", "cmc_rank"]].loc[
+                    : kwargs["num"]
+                ]
+                token_info["date"] = dt.today().date()
+                return token_info
+            elif category == "volume":
+                try:
+                    volume = pd.DataFrame(response["data"]["marketPairs"])[["volumeUsd"]]
+                    volume.columns = [kwargs["symbol"]]
+                    return volume
+                except KeyError as e:
+                    logging.warning(f"{kwargs['symbol']} KeyError: {e}")
+                    return pd.DataFrame()
+    
+            elif category == "market_number":
+                return int(response["data"]["numMarketPairs"])
+            
+            elif category == "listing":
+                return [i['baseSymbol'] for i in response['data']['marketPairs']]
+            
+            elif category == "woo_listing":
+                token = [i["symbol"].split("_")[1].upper() for i in response['rows']]
+                cat = [i["symbol"].split("_")[0].lower() for i in response['rows']]
+                cat = ["perpetual" if i == "perp" else i for i in cat]
+                woo = pd.DataFrame({"symbol": token, "type": cat})
+                woo['exchange'] = "WOO Network"
+                return woo
 
-        elif category == "market_number":
-            return int(response["data"]["numMarketPairs"])
+        except Exception as e:
+            logging.error(f"Error: {e} - Category: {category} - Response: {response}")
+            if category == "token_info":
+                return pd.DataFrame()
+            elif category == "volume":
+                return pd.DataFrame()
+            elif category == "market_number":
+                return 0
+            elif category == "listing":
+                return []
+            
 
     @staticmethod
     def _create_volume_url(
@@ -72,6 +99,13 @@ class CMCGrabber(BaseClient):
         return (
             f"https://api.coinmarketcap.com/data-api/v3/cryptocurrency/market-pairs/latest?"
             f"slug={slug}&start={start}&limit={limit}&category={cat}&centerType=all&sort=cmc_rank_advanced"
+        )
+    @staticmethod
+    def _create_listing_url(
+            slug: str, start: Optional[int] = 1, limit: Optional[int] = 1, cat: Optional[str] = "spot"
+    ) -> str:
+        return (
+            f"https://api.coinmarketcap.com/data-api/v3/exchange/market-pairs/latest?slug={slug}&category={cat}&start={start}&limit={limit}"
         )
 
     def _get_token_info(self, num: int) -> pd.DataFrame:
@@ -133,10 +167,49 @@ class CMCGrabber(BaseClient):
 
         token_info.fillna(0, inplace=True)
         return token_info.set_index("symbol")
+    
+    def _get_listing_info(self, slug: str, cat: Optional[str] = 'spot', limit: Optional[int] = 1000) -> list:
+        market_number = self._get_exchange_market_number(slug=slug, cat=cat)
+        if market_number == 0:
+            return []
+        
+        start = 1
+        results = []
+        while start <= market_number:
+            url = self._create_listing_url(slug=slug, cat=cat, start=start, limit=limit)
+            response = rq.get(url).json()
+            results += self._handle_response(response=response, category="listing")
+            start += limit
+        return list(set(results))
+        
+    def get_listing_info(self) -> pd.DataFrame:
+        exchange_info = self.config[self.EXCHANGE_INFO]
+        
+        results = pd.DataFrame()
+        
+        for cat in ["spot", "perpetual"]:
+            for name, slug in exchange_info.items():
+                logging.info(f"Start getting listing info for {name} - {cat}")
+                tokens = self._get_listing_info(slug=slug, cat=cat)
+                result = pd.DataFrame({"exchange": name, "symbol": tokens, "type": cat})
+                results = pd.concat([results, result], axis=0)
+                
+        return results
+        
+    def _get_exchange_market_number(self, slug: str, cat: str) -> int:
+        url = self._create_listing_url(slug=slug, cat=cat)
+        return self._handle_response(response=rq.get(url).json(), category="market_number")
+    
+    def get_woo_listing(self):
+        response = rq.get(self.WOO_LISTING_URL).json()
+        return self._handle_response(response=response, category="woo_listing")
+        
+        
 
 
 class Tools(BaseClient):
     VOLUME_DB_PATH = os.path.join(BaseClient.CURRENT_PATH, "../db/volume_db.csv")
+    LISTING_DB_PATH = os.path.join(BaseClient.CURRENT_PATH, "../db/listing_db.csv")
     GCS_KEY_PATH = os.path.join(BaseClient.CURRENT_PATH, "gcs_key.json")
     ONLINE_VOLUME_DB_URL = (
         "https://docs.google.com/spreadsheets/d/1wfz0T-dtWScZ1WyrfSY2rjyV6kQ4CiCh07hywLZjLpQ/edit?usp=sharing"
@@ -152,13 +225,20 @@ class Tools(BaseClient):
                 "online": self.gcs.open_by_url(self.ONLINE_VOLUME_DB_URL),
             },
             "listing": {
-            
+                "path": self.LISTING_DB_PATH,
+                "type": "csv",
             }
         }
 
     def _init_volume_db(self):
         if os.path.exists(self.VOLUME_DB_PATH):
             return pd.read_csv(self.VOLUME_DB_PATH).set_index("symbol")
+        else:
+            return pd.DataFrame()
+        
+    def _init_listing_db(self):
+        if os.path.exists(self.LISTING_DB_PATH):
+            return pd.read_csv(self.LISTING_DB_PATH).set_index("symbol")
         else:
             return pd.DataFrame()
 
