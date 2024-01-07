fe280e5302e0ce677c8dc6c59f993f26cababd3c
davdiding <david.ding@woo.network>
2024-01-07 16:50:05 +0000

    Add listing monitor structure

---
+++ listing_monitor/main.py
@@ -0,0 +1,16 @@
+import asyncio
+
+# import pymongo as pm
+# from telegram import Bot
+
+
+class ListingMonitor:
+    pass
+
+    async def run(self):
+        pass
+
+
+if __name__ == "__main__":
+    monitor = ListingMonitor()
+    asyncio.run(monitor.run())
