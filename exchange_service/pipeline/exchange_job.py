7e163502357b016cdd7e40f348082d5cbaf55832
davdiding <david.ding@woo.network>
2024-01-18 11:02:05 +0000

    Refactor exchange_service to a package

--- exchange_service/pipeline/exchange_job.py
+++
@@ -1,9 +0,0 @@
-# exchange_job.py
-
-from abc import ABC, abstractmethod
-
-
-class ExchangeJob(ABC):
-    @abstractmethod
-    async def run(self, **kwargs):
-        raise NotImplementedError
---
+++ exchange_service/setup.py
@@ -0,0 +1,7 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="exchange_service",
+    version="1.0.0",
+    packages=find_packages(),
+)
