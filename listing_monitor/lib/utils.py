d6724f9eeed9652908f3ffedf128b19ac84015df
davdiding <david.ding@woo.network>
2024-01-18 14:15:56 +0000

    Update listing monitor and add update job

---
+++ listing_monitor/lib/utils.py
@@ -0,0 +1,52 @@
+import json
+import logging
+import os
+from datetime import datetime as dt
+
+import pymongo as pm
+
+
+class Tool(object):
+    CURRENT_PATH = os.path.abspath(os.path.dirname(__file__))
+    CONFIG_PATH = os.path.join(CURRENT_PATH, "config.json")
+
+    DB_URL = "MONGO_URL"
+    LOG_MAP = {
+        "main": os.path.join(CURRENT_PATH, "log/main.log"),
+    }
+
+    def __init__(self):
+        self.config = self.init_config()
+        self.client = pm.MongoClient(self.config[self.DB_URL])
+        self.logger = None
+
+    def init_config(self) -> dict:
+        with open(self.CONFIG_PATH) as f:
+            config = json.load(f)
+            f.close()
+        return config
+
+    def init_collection(self, db: str, name: str) -> pm.collection.Collection:
+        return self.client[db][name]
+
+    def get_timestamp(self) -> int:
+        return int(dt.now().timestamp() * 1000)
+
+    def get_datetime(self) -> str:
+        return dt.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    def get_logger(self, name: str) -> logging.Logger:
+
+        logger = logging.getLogger(name)
+
+        formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(filename)s - %(message)s")
+
+        file_handler = logging.FileHandler(self.LOG_MAP[name])
+        file_handler.setFormatter(formatter)
+
+        logger.addHandler(file_handler)
+        logger.setLevel(logging.INFO)
+        logger.propagate = False
+
+        self.logger = logger
+        return self.logger
--- listing_monitor/updater.py
+++ listing_monitor/updater.py
@@ -1,11 +1,18 @@
 import asyncio
 
-from ..exchange_service.lib.binance import Binance
-from ..exchange_service.lib.bybit import Bybit
-from ..exchange_service.lib.okx import Okx
+from lib.utils import Tool
+
+from exchange_service.binance import Binance
+from exchange_service.bybit import Bybit
+from exchange_service.okx import Okx
 
 
 class Updater(object):
+    def __init__(self):
+        self.tool = Tool()
+        self.logger = self.tool.get_logger("main")
+        self.collection = self.tool.init_collection("CexData", "exchange_info")
+
     async def run(self):
         binance = await Binance().create()
         okx = await Okx().create()
@@ -14,8 +21,19 @@ class Updater(object):
         # get exchange_info
         for exchange in [binance, okx, bybit]:
             exchange_info = await exchange.get_exchange_info()
-            print(exchange_info)
-            continue
+            result = {
+                "timestamp": self.tool.get_timestamp(),
+                "datetime": self.tool.get_datetime(),
+                "exchange": exchange.name,
+                "data": exchange_info,
+            }
+            self.collection.insert_one(result)
+
+        self.logger.info("Update exchange_info of binance, okx, bybit")
+
+        await binance.close()
+        await okx.close()
+        await bybit.close()
 
 
 if __name__ == "__main__":
