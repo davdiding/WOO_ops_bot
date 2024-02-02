029029145f6a98836d4ad381b954f74feccb8834
David Ding <davidding@kronosresearch.com>
2024-02-02 08:50:34 +0000

    Add spot API endpoint in kucoin

--- exchange_service/exchanges/kucoin.py
+++ exchange_service/exchanges/kucoin.py
@@ -14,6 +14,28 @@ class KucoinSpot(BaseClient):
     async def _get_symbol_list(self):
         return await self._get(self.spot_base_endpoint + "/api/v2/symbols")
 
+    async def _get_tickers(self):
+        return await self._get(self.spot_base_endpoint + "/api/v1/market/allTickers")
+
+    async def _get_ticker(self, symbol: str):
+        return await self._get(self.spot_base_endpoint + f" /api/v1/market/orderbook/level1?symbol={symbol}")
+
+    async def _get_24hr_stats(self, symbol: str):
+        return await self._get(self.spot_base_endpoint + f"/api/v1/market/stats?symbol={symbol}")
+
+    async def _get_klines(self, symbol: str, type: str, startAt: int = None, endAt: int = None):
+        params = {
+            "symbol": symbol,
+            "type": type,
+        }
+        if startAt:
+            params["startAt"] = startAt
+
+        if endAt:
+            params["endAt"] = endAt
+
+        return await self._get(self.spot_base_endpoint + "/api/v1/market/candles", params=params)
+
 
 class KucoinFutures(BaseClient):
     BASE_ENDPOINT = "https://api-futures.kucoin.com"
@@ -24,3 +46,6 @@ class KucoinFutures(BaseClient):
 
     async def _get_symbol_list(self):
         return await self._get(self.futures_base_endpoint + "/api/v1/contracts/active")
+
+    async def _get_ticker(self, symbol: str):
+        return await self._get(self.futures_base_endpoint + f"/api/v1/ticker?symbol={symbol}")
