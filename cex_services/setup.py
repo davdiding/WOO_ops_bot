a4ba017b17c858670684ddb0a272716477045f32
David Ding <davidding@kronosresearch.com>
2024-02-02 10:58:27 +0000

    Move cex_service to another repo

--- cex_services/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import find_packages, setup
-
-setup(
-    name="cex_services",
-    version="1.0.0",
-    packages=find_packages(),
-)
--- exchange_service/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import find_packages, setup
-
-setup(
-    name="exchange_service",
-    version="1.0.1",
-    packages=find_packages(),
-)
